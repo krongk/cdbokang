d298fe6bc900850426532debc19249d21ba0ed4c
krongk <kenrome@gmail.com>
2012-04-08 23:15:02 +0800

    adfe

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,7 +3,15 @@ class ContactsController < InheritedResources::Base
   before_filter :authenticate_admin_user!, :except => [:index, :show, :new, :create]
 
   def create
-  	flash[:notice] = "信息提交成功！请等待客服人员与您联系。"
-  	super
+  	if params[:contact][:name].blank? || params[:contact][:name] == '您的姓名...' ||
+  		params[:contact][:phone].blank? || params[:contact][:phone]== '您的联系方式...' ||
+  		params[:contact][:content].blank?
+  	  flash[:notice] = "信息提交失败，请检查是否输入正确的信息。"
+  	  redirect_to "/contacts/new"
+  	  return false
+  	else
+	  flash[:notice] = "信息提交成功！请等待客服人员与您联系。"
+	  super
+	end
   end
 end
--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,8 +3,13 @@ class MerchantsController < InheritedResources::Base
   before_filter :authenticate_admin_user!, :except => [:index, :show, :new, :create]
 
   def create
-  	flash[:notice] = "信息提交成功！请等待客服人员与您联系。"
-  	super
+  	if params[:merchant][:name].blank? || params[:merchant][:phone].blank?
+	  	flash[:notice] = "信息提交失败，请检查输入是否正确。"
+	  	redirect_to "/merchants/new"
+	else
+	  flash[:notice] = "信息提交成功！请等待客服人员与您联系。"
+  	  super
+  	end
   end
 
 end 
